8fee45fc5e10b8aefaf36d2e492ac023279ea252
SangMin Park <bak575755@gmail.com>
2025-06-13 15:18:37 +0900

    사진 백업 ㅅㅂ

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  
+  // Body parser 크기 제한 증가 (5MB)
+  app.use(bodyParser.json({ limit: '5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '5mb', extended: true }));
   
   // Swagger 설정
   const config = new DocumentBuilder()
